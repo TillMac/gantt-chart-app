ec79daf62172a1d1f51a4093fa6d71f484467db1
TillMac <danny890308@gmail.com>
2022-09-27 20:49:46 +0800

    chore CI/CD test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload, and text CI/CD for sure!
         </p>
         <a
           className="App-link"
