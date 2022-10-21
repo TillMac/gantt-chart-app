9b3c0848cba2870bba6df5597c30280405d7afa8
TillMac <danny890308@gmail.com>
2022-10-21 19:16:16 +0800

    GC-16 feat: building InputArea component
    原因：
    1. 使用者需要一個地方可以輸入專案項目的各個 task。

--- src/component/InputArea/index.jsx
+++ src/component/InputArea/index.jsx
@@ -1,6 +1,84 @@
+import { Box, FormControl, InputLabel, OutlinedInput, Select, MenuItem, Stack, TextField, Button } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+import { useState } from "react";
+
 const InputArea = () => {
+  const [value, setValue] = useState([null, null]);
+
   return (
-    <div>InputArea</div>
+    <Box
+      sx={{width: '100%', display: 'flex',}}
+    >
+      <Box sx={{
+        width: '90%',
+        height: 180,
+        display: 'flex',
+        backgroundColor: 'white',
+        mt: 2.5,
+        mb: 2.5,
+        mr: 'auto',
+        ml: 'auto',
+        borderRadius: 2,
+        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
+      }}>
+        <form style={{display: 'flex', margin: 'auto', position: 'relative', maxWidth: '867px', flexWrap: 'wrap',}}>
+          <Stack direction='row' spacing={2.5} sx={{position: 'relative'}}>
+            <FormControl sx={{minWidth: '150px',}}>
+              <InputLabel htmlFor="component-outlined">事項名稱</InputLabel>
+              <OutlinedInput
+                id="component-outlined"
+                label="事項名稱"
+                placeholder="輸入事項名稱..."
+              />
+            </FormControl>
+            <FormControl sx={{minWidth: '150px',}}>
+              <InputLabel>Age</InputLabel>
+              <Select
+                label="Age"
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="開始日期"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="截止日期"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Stack>
+          <Button
+            size='large'
+            sx={{
+              maxWidth: '150px',
+              marginLeft: 'auto',
+              mt: 2,
+              backgroundColor: (theme) => theme.palette.other.btn,
+              color: (theme) => theme.palette.other.white,
+            }}
+          >
+            新增事項
+          </Button>
+        </form>
+      </Box>
+    </Box>
   )
 }
 
