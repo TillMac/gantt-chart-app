1b74c903beece29dabd270583c6882a2332ac724
TillMac <danny890308@gmail.com>
2022-11-05 23:09:42 +0800

    GC-27 feat：選擇專案會顯示對應專案 data
    問題：
    1. 先前版本只能一次顯示全部，無法針對個別專案顯示該專案內容。
    2. 時間格式顯示錯誤。
    
    原因：
    1. 沒有處理好 react router <Outlet /> 的 loadingData。
    2. 從 JSON 格式轉換過來，需重新調整時間格式。
    
    解決方法：
    1. 更新 ganttData state 後，一律優先更新至 localstorage，接著透過 react router loader 來綁定對應的 async function，這樣就能讀取 localstorage 的資料。
    2. 在抓取資料的 async function 內直接重新設定各個 task 的時間格式。

--- src/component/GanttArea/index.jsx
+++ src/component/GanttArea/index.jsx
@@ -1,6 +1,8 @@
 import { Box, Divider } from '@mui/material';
 import { Gantt } from 'gantt-task-react';
+import { useLoaderData } from 'react-router-dom';
 import React from 'react'
+import { getGanttData } from '../../functions/getGanttData';
 
 import "gantt-task-react/dist/index.css";
 
@@ -122,10 +124,32 @@ const testData = [
 ]
 
 const GanttArea = () => {
+  const ganttData = useLoaderData();
+  console.log('ganttData from useLoaderData', ganttData);
   return (
     <Box sx={{width: '100%', }}>
       {
-        testData.map((projects, idx) => {
+        (ganttData[0].list.length !== 0) ? (
+          ganttData.map((projects, idx) => {
+            return (
+              <Box key={idx} sx={{maxWidth: '90%', ml: 'auto', mr: 'auto', }}>
+                <h3>{projects.projectName}</h3>
+                <Gantt tasks={projects.list} />
+                <Divider />
+              </Box>
+            )
+          })) : (
+          ganttData.map((projects, idx) => {
+            return (
+              <Box key={idx} sx={{maxWidth: '90%', ml: 'auto', mr: 'auto', }}>
+                <h3>{projects.projectName}</h3>
+                <p>Sorry, no data here!</p>
+                <Divider />
+              </Box>
+            )
+          }))
+      }
+        {/* ganttData.map((projects, idx) => {
           return (
             <Box key={idx} sx={{maxWidth: '90%', ml: 'auto', mr: 'auto', }}>
               <h3>{projects.projectName}</h3>
@@ -133,10 +157,16 @@ const GanttArea = () => {
               <Divider />
             </Box>
           )
-        })
-      }
+        }) */}
+      {/* } */}
     </Box>
   )
 }
 
 export default GanttArea;
+
+export const loader = async({ params }) => {
+  const projectName = params.name;
+  const ganttData = await getGanttData(projectName);
+  return ganttData;
+};
---
+++ src/functions/getGanttData.js
@@ -0,0 +1,19 @@
+export const getGanttData = async(id) => {
+  const ganttDatas = await localStorage.getItem("ganttDatas");
+  const ganttData = JSON.parse(ganttDatas);
+  const result = ganttData.some(project => id === project.name);
+  if (!result) {
+    console.log('something went wrong in the getGanttData session, plz check...');
+  } else {
+    const filteredGanttData = ganttData.filter(project => project.name === id);
+    const dataList = filteredGanttData[0].list;
+    filteredGanttData[0].list = dataList.map(task => {
+      return {
+        ...task,
+        start: new Date(task.start),
+        end: new Date(task.end),
+      }
+    })
+    return filteredGanttData;
+  };
+};
