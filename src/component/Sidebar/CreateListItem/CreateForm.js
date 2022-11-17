fbdafad62590b0b8856d105c5cb8675e3f9942e7
TillMac <danny890308@gmail.com>
2022-11-17 16:01:07 +0800

    GC-33 feat：Project 名稱不可有特殊符號
    問題：
    1. project 名稱若有特殊符號，會影響 router 名稱出現問題，導致後續資料匹配不上等問題。
    
    解決辦法：
    1. 使用正則表達式搭配 MUI <FormControl /> 的 error attribute，以及 <IconButton /> 的 disabled attribute，讓使用者輸入特殊符號時，無法傳送資料，並且 UI 會反紅，告知錯誤。
    2. 使用者只可以輸入：數字、中英文、注音符號、hyphen、空格。

--- src/component/Sidebar/CreateListItem/CreateForm.js
+++ src/component/Sidebar/CreateListItem/CreateForm.js
@@ -15,16 +15,29 @@ import { addProjectIntoGanttData } from '../../../store/ganttDataSlice';
 
 const CreateForm = ({ clickCreate, setClickCreate }) => {
 	const cancelCreateHandler = () => setClickCreate(!clickCreate);
+	const [isInputError, setIsInputError] = useState(false);
 	const dispatch = useDispatch();
 
 	const [catData, setCatData] = useState({});
 
 	const inputHandler = (e) => {
-		setCatData({
-			projectName: e.target.value.trim(),
-			name: e.target.value.replace(/\s*/g, ''),
-			id: uuidv4(),
-		});
+		if (
+			e.target.value.match(
+				/^[\u4e00-\u9fa5_a-zA-Z0-9_\x20_\u3105-\u3129\u02CA\u02C7\u02CB\u02D9_-]+$/
+			) !== null &&
+			e.target.value.trim() !== ''
+		) {
+			setIsInputError(false);
+			setCatData({
+				projectName: e.target.value.trim(),
+				name: e.target.value.replace(/\s*/g, ''),
+				id: uuidv4(),
+			});
+		} else {
+			setIsInputError(true);
+			console.log(e.target.value);
+			console.log('isInputError', isInputError);
+		}
 	};
 
 	const createProjectHandler = (e) => {
@@ -51,18 +64,18 @@ const CreateForm = ({ clickCreate, setClickCreate }) => {
 			<form
 				style={{ display: 'flex', margin: 'auto' }}
 				onSubmit={createProjectHandler}>
-				<FormControl sx={{ maxWidth: '120px', mr: 1.25 }}>
+				<FormControl sx={{ maxWidth: '120px', mr: 1.25 }} error={isInputError}>
 					<InputLabel htmlFor='component-outlined'>新專案名稱</InputLabel>
 					<Input
 						id='component-outlined'
 						label='新事項名稱'
 						placeholder='輸入新事項名稱...'
 						// inputRef={inputProjectRef}
-						onBlur={inputHandler}
+						onChange={inputHandler}
 					/>
 				</FormControl>
 				<ListItem sx={{ pl: 0, pr: 0, maxWidth: '60px' }}>
-					<IconButton size='small' type='submit'>
+					<IconButton size='small' type='submit' disabled={isInputError}>
 						<Add />
 					</IconButton>
 					<IconButton size='small' onClick={cancelCreateHandler}>
