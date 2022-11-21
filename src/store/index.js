a4ff029b8914002e64b69f10006c3cc80a627131
TillMac <danny890308@gmail.com>
2022-11-21 21:32:32 +0800

    GC-71 feat：可以調整時間刻度
    問題：
    1. 使用者需要使用滾軸才能查看該專案的全部甘特圖，若 task 時間過長無法一次看清全貌
    
    原因：
    1. 沒有設定 gantt-react-task 的 <Gantt> 時間刻度 viewMode。
    
    解決方法：
    1. 建立 timeView slice；
    2. 設立選單可以設定 timeView，預設為 'Day' 模式。

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectCatReducer from './projectCatSlice';
 import ganttDataSlice from './ganttDataSlice';
+import timeViewSlice from './timeViewSlice';
 
 export const store = configureStore({
 	reducer: {
 		projectCategories: projectCatReducer,
 		ganttDataRedux: ganttDataSlice,
+		timeView: timeViewSlice,
 	},
 });
