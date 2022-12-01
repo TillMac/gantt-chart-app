e3b701705f1c7cf0dc6a862681ba57471e40b271
TillMac <danny890308@gmail.com>
2022-12-01 18:41:38 +0800

    GC-77 feat：刪除 Project Cat & task 皆會刪除資料庫對應 task(s)
    問題：
    1. 使用者刪除 task 時，DynamoDB 對應資料不會跟著刪除。
    2. 使用者刪除 Project Cat 時，DynamoDB 對應資料不會跟著刪除。
    
    原因：
    1. 沒有設定 delete API
    
    解決方法：
    1. 設定 delete API。
    2. 設定 lamda function。

--- amplify/backend/function/ganttTasksFunc/src/app.js
+++ amplify/backend/function/ganttTasksFunc/src/app.js
@@ -149,20 +149,65 @@ app.get(path + '/object' + hashKeyPath + sortKeyPath, function (req, res) {
  * HTTP put method for insert object *
  *************************************/
 
-app.put(path, function (req, res) {
-	if (userIdPresent) {
-		req.body['userId'] =
+app.put(path + hashKeyPath + '/:id', function (req, res) {
+	const params = {
+		id: req.params.id,
+	};
+	// if (userIdPresent) {
+	// 	req.body['userId'] =
+	// 		req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
+	// }
+
+	if (userIdPresent && req.apiGateway) {
+		params[partitionKeyName] =
 			req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
+	} else {
+		params[partitionKeyName] = req.params[partitionKeyName];
+		try {
+			params[partitionKeyName] = convertUrlType(
+				req.params[partitionKeyName],
+				partitionKeyType
+			);
+		} catch (err) {
+			res.statusCode = 500;
+			res.json({ error: 'Wrong column type ' + err });
+		}
+	}
+	if (hasSortKey) {
+		try {
+			params[sortKeyName] = convertUrlType(
+				req.params[sortKeyName],
+				sortKeyType
+			);
+		} catch (err) {
+			res.statusCode = 500;
+			res.json({ error: 'Wrong column type ' + err });
+		}
 	}
 
 	const timestamp = new Date().toJSON();
-	req.body.createdAt = timestamp;
 	req.body.updatedAt = timestamp;
 
 	let putItemParams = {
 		TableName: tableName,
-		Item: req.body,
+		Key: params,
+		// Item: req.body,
+		UpdateExpression: 'SET #name = :n, #updatedAt = :u',
+		ExpressionAttributeNames: {
+			'#name': 'name',
+			'#updatedAt': 'updatedAt',
+		},
+		ExpressionAttributeValues: {
+			':n': req.body.name,
+			':u': req.body.updatedAt,
+		},
+		ReturnValues: 'UPDATED_NEW',
 	};
+
+	// let putItemParams = {
+	// 	TableName: tableName,
+	// 	Item: req.body,
+	// };
 	dynamodb.put(putItemParams, (err, data) => {
 		if (err) {
 			res.statusCode = 500;
@@ -183,6 +228,10 @@ app.post(path, function (req, res) {
 			req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
 	}
 
+	const timestamp = new Date().toJSON();
+	req.body.createdAt = timestamp;
+	req.body.updatedAt = timestamp;
+
 	let putItemParams = {
 		TableName: tableName,
 		Item: req.body,
@@ -201,8 +250,11 @@ app.post(path, function (req, res) {
  * HTTP remove method to delete object *
  ***************************************/
 
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function (req, res) {
-	const params = {};
+// app.delete(path + '/object' + hashKeyPath + sortKeyPath, function (req, res) {
+app.delete(path + hashKeyPath + '/:id', function (req, res) {
+	const params = {
+		id: req.params.id,
+	};
 	if (userIdPresent && req.apiGateway) {
 		params[partitionKeyName] =
 			req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
--- src/component/Sidebar/ChangeList/DeleteButton.js
+++ src/component/Sidebar/ChangeList/DeleteButton.js
@@ -2,18 +2,27 @@ import { DeleteOutline } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import { API } from 'aws-amplify';
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteProjectFromGanttData } from '../../../store/ganttDataSlice';
 import { deleteProjectCat } from '../../../store/projectCatSlice';
 
 export default function DeleteButton({ project }) {
 	const dispatch = useDispatch();
+	const ganttTasksData = useSelector((state) => state.ganttDataRedux);
 	const catDeleteHandler = (e) => {
 		console.log('try to delete so hard...');
 		e.preventDefault();
-		API.del('projectCatsApi', `/projectcats/userId/${project.id}`, {
-			response: true,
-		}).then((response) => console.log(response, 'from del API'));
+		// API.del('projectCatsApi', `/projectcats/userId/${project.id}`, {
+		// 	response: true,
+		// }).then((response) => console.log(response, 'from del cat API'));
+		const delProjectData = ganttTasksData.projects.filter(
+			(thisProject) => thisProject.id === project.id
+		);
+		delProjectData[0].list.forEach((task) => {
+			API.del('ganttTasksApi', `/gantttasks/userId/${task.id}`, {
+				response: true,
+			}).then((response) => console.log(response, 'from del task API'));
+		});
 		dispatch(deleteProjectCat(project.id));
 		dispatch(deleteProjectFromGanttData(project.id));
 	};
